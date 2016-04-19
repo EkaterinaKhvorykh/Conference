97339699f710ca1e46e21acad15d15d4d1863db7
Milenina <5346258@gmail.com>
2016-04-19 11:58:22 +0300

    Background

--- App/ViewController.swift
+++ App/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png"))
         // Do any additional setup after loading the view, typically from a nib.
+        let backgroundImage = UIImageView(frame: UIScreen.mainScreen().bounds)
+        backgroundImage.image = UIImage(named: "back2.jpg")
+        self.view.insertSubview(backgroundImage, atIndex: 0)
     }
 
     override func didReceiveMemoryWarning() {
